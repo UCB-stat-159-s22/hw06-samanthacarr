2744623ac35a3e60f7ea283d10ede26ecc0bd941
Samantha Carr <samantha.carr@berkeley.edu>
2022-04-25 02:40:21 +0000

    completed up through utils functions

---
+++ ligotools/tests/test_ligotools.py
@@ -0,0 +1,8 @@
+from ligotools import *
+
+def test_ligo_default():
+    assert 1 == 1
+
+def test_2():
+    assert 1 == 1
+
---
+++ ligotools/utils/__init__.py
@@ -0,0 +1,8 @@
+"""
+The utils package.
+"""
+
+__version__ = "0.0.1"
+
+from .utils import *
+from.plotting_help import *
